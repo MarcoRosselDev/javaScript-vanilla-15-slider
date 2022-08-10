9d9ce9fb11079283f76f91a16dbae5015f55c489
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-10 05:24:05 -0400

    javaScript 2.2

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const nextBtn = document.querySelector('.nextBtn');
 const prevBtn = document.querySelector('.prevBtn');
 
 slides.forEach(function(slide, index){
-  slide.style.left = `${index * 100}%`
+  slide.style.left = `${index * 100}%`;
 });
 
 let counter = 0;
 
 nextBtn.addEventListener('click', function(){
   counter++;
+  carousel();
 });
 
 prevBtn.addEventListener('click', function(){
   counter--;
+  carousel();
 });
 
 function carousel(){
   slides.forEach(function(slide){
-    slide.style.transform = `translateX(-${counter})`
+    slide.style.transform = `translateX(-${counter * 100}%)`
   });
 }
